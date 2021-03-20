66045a37cd98c67fb6b93a3ea04f41541c70d27f
patelvishwa112 <patelvishwa112@gmail.com>
2021-03-20 18:37:09 -0500

    Create MVC Design

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,11 +14,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    var storyBrain = StoryBrain()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        updateUI()
     }
 
 
+    @IBAction func choiceMade(_ sender: UIButton) {
+        
+        // Check which button was pressed
+        let buttonPressed = sender.currentTitle!
+        storyBrain.nextStory(choice: buttonPressed)
+        
+        updateUI()
+        
+    }
+    
+    func updateUI() {
+        let newStory = storyBrain.getNewStory()
+        
+        storyLabel.text = newStory.title
+        choice1Button.setTitle(newStory.choice1, for: .normal)
+        choice2Button.setTitle(newStory.choice2, for: .normal)
+ 
+
+    }
 }
 
